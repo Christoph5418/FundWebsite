9fdd91eab9679168d95f688af75e2edc98248da1
Chris Fadel <90206880+Christoph5418@users.noreply.github.com>
2024-02-06 14:16:18 -0500

    Add files via upload

--- src/app/script.py
+++ src/app/script.py
@@ -3,12 +3,11 @@ from flask_cors import CORS
 import yfinance as yf
 from datetime import datetime, timedelta
 from requests import get
+from flask_sslify import SSLify  # Install this package if not already installed
 import requests
-import csv
-from io import StringIO
 
 app = Flask(__name__)
-
+sslify = SSLify(app)
 
 CORS(app)
 
@@ -22,7 +21,6 @@ def get_stock_data(symbol):
     except Exception as e:
         print("Error occurred:", e)
         return None
-    
 
 @app.route('/stock-data/<ticker>')
 def get_stock_api(ticker):
@@ -31,27 +29,16 @@ def get_stock_api(ticker):
         return jsonify(stock_data)
     else:
         return jsonify(error="Error occurred while retrieving stock data for the ticker symbol."), 500
-    
+
 @app.route('/sheets')
 def get_sheets():
-    url = 'https://docs.google.com/spreadsheets/d/1k6qrzXNFCBD_UJ9K6VqbqnYR6WkzTBVFTJ-lBE_6shU/export?format=tsv&id=1k6qrzXNFCBD_UJ9K6VqbqnYR6WkzTBVFTJ-lBE_6shU&gid=372495094'
-    r = requests.get(url)
+    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-ovEH2cjjx4p1cj49SLwRWZAA8coXB_RLsdxqbzT1AcWpxY5DU4zbamRptFjUcbgmjSqXqM7kKhLK/pub?output=tsv&gid=372495094'
+    r = requests.get(url)  # Use requests.get here
     if r.status_code == 200:
         data = r.text
-
-        # Use StringIO to turn the text data into a file-like object
-        tsv_data = StringIO(data)
-
-        # Parse the TSV data
-        reader = csv.reader(tsv_data, delimiter='\t')
-
-        # Convert reader to a list of lists (2D array)
-        data_2d_array = [row for row in reader]
-        
-        return jsonify(data_2d_array)
+        return data
     else:
-        return jsonify(error="Error occurred while fetching data from Google Sheets")
+        return jsonify(error="Error occurred while fetching data from Google Sheets."), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
-
+    app.run(host='0.0.0.0', port=5001, ssl_context=('/etc/letsencrypt/live/nittanylionfundportfoliorisk.com/cert.pem', '/etc/letsencrypt/live/nittanylionfundportfoliorisk.com/privkey.pem'))
